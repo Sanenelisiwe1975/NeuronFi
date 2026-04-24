f0d049c49b61743f4845232669ce286008303da6
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-04-24 11:19:26 +0200

    update

---
+++ packages/kite/src/index.ts
@@ -0,0 +1,6 @@
+// Kite Agent Passport integration
+// Provides agent identity, session management, and spending controls
+
+export * from "./types.js";
+export { KitePassport, initializePassport, getPassport } from "./passport.js";
+export * from "./session.js";
