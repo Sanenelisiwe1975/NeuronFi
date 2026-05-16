e05da57ade802a1d990c7bfd8895893bb109a299
sanenelisiwe1975 <Sanelisiwesimatu@gmail.com>
2026-05-16 09:51:43 +0200

    imports update

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState, useEffect, useCallback, useRef, useMemo } from "react";
-import { TradeTable } from "../../components/TradeTable";
-import { PortfolioChart } from "../../components/PortfolioChart";
+import { TradeTable } from "components/TradeTable";
+import { PortfolioChart } from "components/PortfolioChart";
 
 
 interface AgentState {
